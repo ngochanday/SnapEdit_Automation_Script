1f52fb9fb0754341ce4b546f1f8bdb09e03db830
ngochanday <hanvan1331@gmail.com>
2023-09-25 13:40:16 +0700

    commit 1

--- page-objects/EnhancerPage.js
+++ page-objects/EnhancerPage.js
@@ -5,6 +5,8 @@ export class EnhancerPage {
         this.page = page
         this.prooductsDropDown = page.locator('[class="H14B inline-flex items-center space-x-1.5 cursor-pointer px-3 py-2 rounded-lg transition hover:bg-blue-100 hover:text-blue-500 bg-blue-100 text-blue-500"]')
         this.enhancerButton = page.locator('button:has-text("Enhancer")')
+        this.enhancerDemoImage = page.locator('li.inline-block button')
+        this.enhancerDownloadButton = page.locator('[class="inline-flex items-center justify-center w-full py-3 px-4 bg-blue-500 rounded-lg text-base transition text-white hover:bg-opacity-80"]')
     }
 
     goToEnhancerPage = async () => {
@@ -14,12 +16,40 @@ export class EnhancerPage {
         //Hover on products dropdown
         await this.prooductsDropDown.waitFor()
         await this.prooductsDropDown.click()
-        //wait the time out to keep the hover for 2 second
-        await this.page.waitForTimeout(3000);
+        //wait the time out to hold on the button for 3 second
+        await this.page.waitForTimeout(2000);
 
         //Click on Enhancer button in Dropdown list
         await this.enhancerButton.waitFor()
         await this.enhancerButton.click()
 
+    }
+
+    goToEnhancerEditPage = async () => {
+        const enhancerDemoImage =  this.enhancerDemoImage.first()
+
+        await enhancerDemoImage.waitFor()
+        await enhancerDemoImage.click()
+    }
+
+    downloadEnhancerFile = async () => {
+        await this.enhancerDownloadButton.waitFor()
+        // Trigger the download action -> click on DownloadHD button
+        await this.enhancerDownloadButton.click()
+
+        //Wait for the download event 
+        const [download] = await Promise.all([
+            //after triggering the download should wait for the download to be detectd by Playwright
+            this.page.waitForEvent('download'),
+        ]);
+
+        //Access to the download file information
+        const downloadedFilePath = download.path();
+        console.log(`Downloaded file path: ${downloadedFilePath}`)
+        const downloadedFileName = download.suggestedFilename()
+        console.log(`Downloaded file name: ${downloadedFileName}`)
+
+        // Assertions download file path is not null -> if not null it mean file download successful = passed
+        expect(downloadedFilePath).not.toBeNull(); // Ensure a file path is available.
     }
 }
--- tests/user_full_journey.spec.js
+++ tests/user_full_journey.spec.js
@@ -2,33 +2,45 @@ import { test } from "@playwright/test"
 import { RemoveObjectsPage } from "../page-objects/RemoveObjectsPage"
 import { EnhancerPage } from "../page-objects/EnhancerPage"
 
-test('Verify Remove Objects feature work correctly', async ({ page }) => {
-    const removeObjectsPage = new RemoveObjectsPage(page)
-    removeObjectsPage.visit()
-
-    await page.pause()
-
-    removeObjectsPage.goToEditPage()
-
-    await page.pause()
-
-    removeObjectsPage.useAutoAI()
-
-    await page.pause()
-
-    removeObjectsPage.removeObjects()
-
-    await page.pause()
-
-    removeObjectsPage.downloadFile()
-
-    await page.pause()
+test.describe('suite 1', () => {
+    test('Verify Remove Objects feature work correctly', async ({ page }) => {
+        const removeObjectsPage = new RemoveObjectsPage(page)
+        removeObjectsPage.visit()
+    
+        await page.pause()
+    
+        removeObjectsPage.goToEditPage()
+    
+        await page.pause()
+    
+        removeObjectsPage.useAutoAI()
+    
+        await page.pause()
+    
+        removeObjectsPage.removeObjects()
+    
+        await page.pause()
+    
+        removeObjectsPage.downloadFile()
+    
+        await page.pause()
+    })
+    
+    test.skip('Verify Enhancer feature work correctly', async ({ page }) => {
+        const enhancerPage = new EnhancerPage(page)
+        enhancerPage.goToEnhancerPage()
+    
+        await page.pause()
+    
+        enhancerPage.goToEnhancerEditPage()
+    
+        await page.pause()
+    
+        enhancerPage.downloadEnhancerFile()
+    
+        await page.pause()
+    
+    })
 })
 
-test('Verify Enhancer feature work correctly', async ({ page }) => {
-    const enhancer = new EnhancerPage(page)
-    enhancer.goToEnhancerPage()
 
-    await page.pause()
-
-})
